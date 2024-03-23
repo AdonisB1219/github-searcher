9ed120f160a3fcb20843942ab81f3a38c76daee8
Adonis <adonisbecerra1219@gmail.com>
2024-03-23 14:34:56 -0600

    Agregar config deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://AdonisB1219.github.io/github-searcher'
 })
